9127b9fe5293d87b1c7f7a0575c63f09a5eae361
POKOMAN <gamepoko56@gmail.com>
2025-07-30 14:26:00 +0700

    use config file for app name

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+import config
+
 def say_hello(name):
-   print(f"Hello, {name}!")
+   print(f"Hello, {name} from {config.APP_NAME}!")
 
 def greet_user():
    name = input("Please enter your name: ")
